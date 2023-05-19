67f19ba601b43b34b78dbd16427f8076baf9c0c4
Limprog <ilinivan1602@gmail.com>
2023-05-20 00:54:00 +0300

    new dev

--- app.py
+++ app.py
@@ -5,11 +5,21 @@ from new_def import *
 # import dash_design_kit as ddk
 import os
 data_Y = pd.read_csv("data/Участники anonimized.csv")
-data = pd.DataFrame()
+data = pd.read_csv("data/Anonimized/Аддитивные технологии.csv")
 com_competihon = ['Геодезия.csv', 'Мехатроника.csv', 'Управление качеством.csv', 'Технологии композитов.csv', 'Строительный контроль.csv', 'Инженерное проектирование.csv', 'Информационная безопасность.csv', 'Управление жизненным циклом.csv', 'Бетонные строительные работы.csv', 'Инженерное мышление. Каракури.csv', 'Промышленная механика и монтаж.csv', 'Программные решения для бизнеса.csv', 'Сетевое и системное администрирование.csv', 'Цифровое ПСР-Предприятие (Lean Smart Plant).csv', 'Вывод из эксплуатации объектов использования атомной энергии.csv', 'Корпоративная защита от внутренних угроз информационной безопасности.csv']
-
-
-
+col = ["Компетенция"]
+print(list(data.dtypes))
+for i in os.listdir("data/Anonimized"):
+    data = pd.read_csv("data/Anonimized/"+i)
+    comma_cols = [col for col in data.columns if data[col].dtype == 'object' and data[col].str.contains(',').any()]
+    for j in comma_cols:
+        if j !='Место работы':
+            data[j] = data[j].str.replace(',', '.')
+            try:
+                data[j] = data[j].astype(float)
+            except ValueError:
+                pass
+    data.to_csv("data2/Anonimized/"+i)
 
 
 
