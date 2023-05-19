906e4473d233ed7b1789d1aeeb04d0bab81fc915
Limprog <ilinivan1602@gmail.com>
2023-05-19 21:45:56 +0300

    new def

--- new_def.py
+++ new_def.py
@@ -7,3 +7,12 @@ def fio_meng(data):
         # data_temp = data_temp.drop(['ФИО участников'], axis=1)
         data = data.join(data_temp)
         return data
+
+
+def com(path):
+    data = pd.read_csv(path)
+    if set(['ФИО участников']).issubset(data.columns):
+        name = path.split("/")[-1]
+        return name
+    else:
+        pass
