9483bd304becb080b874540939306c06a660a1e5
Limprog <ilinivan1602@gmail.com>
2023-05-19 21:32:50 +0300

    new def

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
-import dash_design_kit as ddk
+# import dash_design_kit as ddk
+import os
+data_Y = pd.read_csv("data/Участники anonimized.csv")
+data = pd.DataFrame()
+
+
---
+++ new_def.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def fio_meng(data):
+    if set(['ФИО участников']).issubset(data.columns):
+        data_temp = data['ФИО участников'].str.split(";", expand=True)
+        data_temp.columns = ['ФИО{}'.format(x + 1) for x in data_temp.columns]
+        # data_temp = data_temp.drop(['ФИО участников'], axis=1)
+        data = data.join(data_temp)
+        return data
